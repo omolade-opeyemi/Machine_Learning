6581f6a51c9da27379dc08bd4a93f3cba3df75e9
opeyemi <opelium@gmail.com>
2021-12-24 09:50:24 +0100

    debug

--- ML/settings.py
+++ ML/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-665*uu80*!^87b5v(gi8^6zt+a*)ck+knf_grctenprg-lo=j2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
